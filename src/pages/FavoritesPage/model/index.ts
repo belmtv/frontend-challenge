e1bd648e8dab53491b3320c6adc7e9a7c806405d
Матвей Белавин <m.belavin@sbereducation.ru>
2024-01-30 16:23:32 +0300

    feat: правки

--- src/pages/FavoritesPage/model/index.ts
+++ src/pages/FavoritesPage/model/index.ts
@@ -2,5 +2,3 @@ import { createStore } from 'effector'
 import { Cat } from '../../MainPage/model/types'
 
 export const $favoriteCards = createStore<Cat[] | null>(null)
-
-$favoriteCards.watch((el) => console.log(el))
